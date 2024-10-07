f8cf1779459d7acb3e24fa4f9d499a7a96541e9e
harshachandragiri <152676877+harshachandragiri@users.noreply.github.com>
2024-10-07 16:01:09 +0530

    node-07oct

---
+++ Built_in/FS_async/stream.js
@@ -0,0 +1,19 @@
+const fs=require('fs');
+
+//Readable Stream
+
+// let readData=fs.createReadStream('./stream1.js','utf-8')
+// readData.on('data',data=>{
+//     console.log(data);
+// })
+
+// readData.on('end',_=>{
+//     console.log('Reading is finished');
+// })
+// readData.on('error',_=>{
+//     console.log('All Events are Closed');
+// })
+
+// readData.on('close',_=>{
+//     console.log('all Events are closed');
+// })
